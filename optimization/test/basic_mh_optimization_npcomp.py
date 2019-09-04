daef16bd65329f886b1364a5eeb56e961e3e280f
Erick Merino <erickfmm@gmail.com>
2019-09-04 00:33:28 -0400

    refactoring split nphard problems

--- optimization/test/basic_mh_optimization_npcomp.py
+++ optimization/test/basic_mh_optimization_npcomp.py
@@ -10,121 +10,8 @@ from optimization.population.SillyRandom.GreedyMH_Real_WithLeap import GreedyMH_
 from optimization.population.PSO.PSOMH_Real import PSOMH_Real
 from optimization.population.PSO.PSOMH_Real_WithLeap import PSOMH_Real_WithLeap
 
-import numpy as np
 
-from numpy.random import RandomState
-
-
-rnd = RandomState(0)
-mydata = rnd.uniform(-100, 100, 200)
-#mydata = rnd.randint(-100, 100, 200)
-ndim = len(mydata)+2
-min_x = 0
-max_x = len(mydata)
-
-def is_valid(point):
-    are_1s = [0 for _ in range(len(mydata))]
-    for i in range(2, len(point)):
-        if point[i] < 0 or int(point[i]) >= len(mydata):
-            return False
-        are_1s[int(point[i])] += 1
-        if are_1s[int(point[i])] > 1:
-            return False
-    for el in are_1s:
-        if el == 0 or el > 1:
-            return False
-    return True
-        
-
-def partition_problem_obj(point):
-    if not is_valid(point):
-        return False
-    s1 = point[0]
-    s2 = point[1]
-    s1 = int(s1/float(s1+s2)*len(mydata))
-    s1 = s1 if s1 >= 0 and s1 < len(mydata) else 0# s1 if s1 > 0 and s1 < len(mydata) else 1
-    s2 = len(mydata)-s1
-    sum1 = 0
-    sum2 = 0
-    for i in range(2, s1+2, 1):
-        sum1 += mydata[int(point[i])]
-    for i in range(s1+2, len(point), 1):
-        sum2 += mydata[int(point[i])]
-    return abs(sum1-sum2)
-
-def subsetsum_problem_obj(point):
-    if not is_valid(point):
-        return False
-    s1 = point[0]
-    s2 = point[1]
-    s1 = int(s1/float(s1+s2)*len(mydata))
-    s1 = s1 if s1 >= 0 and s1 < len(mydata) else 0
-    sum1 = 0
-    for i in range(2, s1+2, 1):
-        sum1 += mydata[int(point[i])]
-    return np.abs(sum1)
-
-def repair_partition(point):
-    point = np.abs(point)
-    are_not = []
-    are_1s = [0 for _ in range(len(mydata))]
-    #dups = []
-    dups = [[] for _ in range(len(mydata))]
-    for i in range(2, len(point)):
-        if int(point[i]) < 0 or int(point[i]) >= len(mydata):
-            point[i] = 0
-        are_1s[int(point[i])] += 1
-        dups[int(point[i])].append(i)
-    for i in range(len(are_1s)):
-        if are_1s[i] == 0:
-            are_not.append(i)
-    sum_dups = []
-    for i in range(len(dups)):
-        sum_dups.append(len(dups[i]))
-        if len(dups[i]) > 0:
-            del dups[i][0]
-    dups = [e for di in dups for e in di] #flatten
-    if len(are_not) != len(dups) or are_1s != sum_dups:
-        print("rayooooooooooos son distintos")
-        print("arenot: ", len(are_not), ", dups: ", len(dups))
-        print("are not: ", are_not)
-        print("dups: ", dups)
-        print("are1s: ", are_1s)
-        point2 = [int(e) for e in point]
-        print("point int: ", point2)
-        print("point: ", point)
-        raise Exception("son distintos :c")
-    for i in range(len(dups)):
-        point[dups[i]] = are_not[i]
-    return point
-
-def preproc_partition(point):
-    point2 = np.abs(np.copy(point))
-    for i in range(2, len(point2)):
-        point2[i] = int(point2[i])
-    return point2
-
-
-
-def print_difference_subsets(point, data):
-    s1 = point[0]
-    s2 = point[1]
-    s1 = int(s1/float(s1+s2)*len(data))
-    s1 = s1 if s1 >= 0 and s1 < len(data) else 0
-    s2 = len(data)-s1
-    sum1 = 0
-    sum2 = 0
-    sub1 = []
-    sub2 = []
-    for i in range(2, s1+2, 1):
-        sum1 += data[int(point[i])]
-        sub1.append(data[int(point[i])])
-    for i in range(s1+2, len(point), 1):
-        sum2 += data[int(point[i])]
-        sub2.append(data[int(point[i])])
-
-    print("sum1:", sum1)
-    print("sum2:", sum2)
+from optimization.problems.nphard_real.partition__and_subset_sum import Partition_Real, Subset_Real
 
 to_use = [
     "AFSA",
@@ -134,49 +21,87 @@ to_use = [
     "PSOWL"
     ]
 
+partition_problem = Partition_Real(seed=0, num_dims=200)
+subset_problem = Subset_Real(seed=0, num_dims=200)
+
+
+problem_to_solve = "partition sum"
+#problem_to_solve = "subset sum"
+
+to_verbose = False
+
 if "AFSA" in to_use:
     print("create afsa")
-    mh = AFSAMH_Real(min_x, max_x, ndim, False, partition_problem_obj, repair_partition, preproc_partition)
-    #mh = AFSA(min_x, max_x, ndim, False, subsetsum_problem_obj, repair_partition, preproc_partition)
+    if problem_to_solve == "partition sum":
+        mh = AFSAMH_Real(partition_problem.min_x, partition_problem.max_x, partition_problem.ndim, False, partition_problem.objective_function, partition_problem.repair_function, partition_problem.preprocess_function)
+    if problem_to_solve == "subset sum":
+        mh = AFSAMH_Real(subset_problem.min_x, subset_problem.max_x, subset_problem.ndim, False, subset_problem.objective_function, subset_problem.repair_function, subset_problem.preprocess_function)
     print("to run afsa")
-    fit, pt = mh.run(verbose=True, visual_distance_percentage=0.5, velocity_percentage=0.5, n_points_to_choose=3, crowded_percentage=0.7, its_stagnation=4, leap_percentage=0.3, stagnation_variation=0.4, seed=115)
+    fit, pt = mh.run(verbose=to_verbose, visual_distance_percentage=0.5, velocity_percentage=0.5, n_points_to_choose=3, crowded_percentage=0.7, its_stagnation=4, leap_percentage=0.3, stagnation_variation=0.4, seed=115)
     print(fit)
     print(pt)
-    print_difference_subsets(pt, mydata)
+    if problem_to_solve == "partition sum":
+        partition_problem.print_difference_subsets(pt)
+    if problem_to_solve == "subset sum":
+        subset_problem.print_difference_subsets(pt)
 
 if "GreedyWL" in to_use:
-    print("create Greedy")
-    mh = GreedyMH_Real_WithLeap(min_x, max_x, ndim, False, partition_problem_obj, repair_partition, preproc_partition)
+    print("create GreedyWL")
+    if problem_to_solve == "partition sum":
+        mh = GreedyMH_Real_WithLeap(partition_problem.min_x, partition_problem.max_x, partition_problem.ndim, False, partition_problem.objective_function, partition_problem.repair_function, partition_problem.preprocess_function)
+    if problem_to_solve == "subset sum":
+        mh = GreedyMH_Real_WithLeap(subset_problem.min_x, subset_problem.max_x, subset_problem.ndim, False, subset_problem.objective_function, subset_problem.repair_function, subset_problem.preprocess_function)
     print("to run greedy")
-    fit, pt = mh.run(verbose=True, iterations=100, population=30, stagnation_variation=0.4, its_stagnation=5, leap_percentage=0.8, seed=115)
+    fit, pt = mh.run(verbose=to_verbose, iterations=100, population=30, stagnation_variation=0.4, its_stagnation=5, leap_percentage=0.8, seed=115)
     print(fit)
     print(pt)
-    print_difference_subsets(pt, mydata)
+    if problem_to_solve == "partition sum":
+        partition_problem.print_difference_subsets(pt)
+    if problem_to_solve == "subset sum":
+        subset_problem.print_difference_subsets(pt)
 
 if "Greedy" in to_use:
     print("create Greedy")
-    mh = GreedyMH_Real(min_x, max_x, ndim, False, partition_problem_obj, repair_partition, preproc_partition)
+    if problem_to_solve == "partition sum":
+        mh = GreedyMH_Real(partition_problem.min_x, partition_problem.max_x, partition_problem.ndim, False, partition_problem.objective_function, partition_problem.repair_function, partition_problem.preprocess_function)
+    if problem_to_solve == "subset sum":
+        mh = GreedyMH_Real(subset_problem.min_x, subset_problem.max_x, subset_problem.ndim, False, subset_problem.objective_function, subset_problem.repair_function, subset_problem.preprocess_function)
     print("to run greedy")
-    fit, pt = mh.run(verbose=True, iterations=100, population=30, seed=115)
+    fit, pt = mh.run(verbose=to_verbose, iterations=100, population=30, seed=115)
     print(fit)
     print(pt)
-    print_difference_subsets(pt, mydata)
+    if problem_to_solve == "partition sum":
+        partition_problem.print_difference_subsets(pt)
+    if problem_to_solve == "subset sum":
+        subset_problem.print_difference_subsets(pt)
 
 if "PSO" in to_use:
     print("create PSO")
-    mh = PSOMH_Real(min_x, max_x, ndim, False, partition_problem_obj, repair_partition, preproc_partition)
+    if problem_to_solve == "partition sum":
+        mh = PSOMH_Real(partition_problem.min_x, partition_problem.max_x, partition_problem.ndim, False, partition_problem.objective_function, partition_problem.repair_function, partition_problem.preprocess_function)
+    if problem_to_solve == "subset sum":
+        mh = PSOMH_Real(subset_problem.min_x, subset_problem.max_x, subset_problem.ndim, False, subset_problem.objective_function, subset_problem.repair_function, subset_problem.preprocess_function)
     print("to run PSO")
-    fit, pt = mh.run(verbose=True, iterations=100, population=30, omega=0.8, phi_g=1, phi_p=0.5 ,seed=115)
+    fit, pt = mh.run(verbose=to_verbose, iterations=100, population=30, omega=0.8, phi_g=1, phi_p=0.5 ,seed=115)
     print(fit)
     print(pt)
-    print_difference_subsets(pt, mydata)
+    if problem_to_solve == "partition sum":
+        partition_problem.print_difference_subsets(pt)
+    if problem_to_solve == "subset sum":
+        subset_problem.print_difference_subsets(pt)
 
 if "PSOWL" in to_use:
-    print("create PSO")
-    mh = PSOMH_Real_WithLeap(min_x, max_x, ndim, False, partition_problem_obj, repair_partition, preproc_partition)
+    print("create PSOWL")
+    if problem_to_solve == "partition sum":
+        mh = PSOMH_Real_WithLeap(partition_problem.min_x, partition_problem.max_x, partition_problem.ndim, False, partition_problem.objective_function, partition_problem.repair_function, partition_problem.preprocess_function)
+    if problem_to_solve == "subset sum":
+        mh = PSOMH_Real_WithLeap(subset_problem.min_x, subset_problem.max_x, subset_problem.ndim, False, subset_problem.objective_function, subset_problem.repair_function, subset_problem.preprocess_function)
     print("to run PSO")
-    fit, pt = mh.run(verbose=True, iterations=100, population=30, omega=0.8, phi_g=1, phi_p=0.5 ,seed=115, stagnation_variation=0.4, its_stagnation=5, leap_percentage=0.8)
+    fit, pt = mh.run(verbose=to_verbose, iterations=100, population=30, omega=0.8, phi_g=1, phi_p=0.5 ,seed=115, stagnation_variation=0.4, its_stagnation=5, leap_percentage=0.8)
     print(fit)
     print(pt)
-    print_difference_subsets(pt, mydata)
+    if problem_to_solve == "partition sum":
+        partition_problem.print_difference_subsets(pt)
+    if problem_to_solve == "subset sum":
+        subset_problem.print_difference_subsets(pt)
     
--- optimization/test/categorical_mh_optimization.py
+++ optimization/test/categorical_mh_optimization.py
@@ -8,6 +8,8 @@ sys.path.append(abspath(join(dirname(__file__), '../..')))
 from optimization.population.Genetic.GeneticMH_Categorical import GeneticMH_Categorical
 from optimization.population.Genetic.GeneticMH_Categorical_WithLeap import GeneticMH_Categorical_WithLeap
 
+from optimization.problems.nphard_categorical.knapsack import Knapsack_Categorical
+
 #import numpy as np
 from numpy.random import RandomState
 from typing import List, Callable, Tuple
@@ -16,86 +18,13 @@ print("hello it is categorical mh test")
 #general parameters
 knapsack_capacity:float = 50.8
 total_posible_elements:int = 50
-seed = 0
-max_value = 5
-max_cost = 6
-rnd = RandomState(seed)
 
-#list of tuples with value and cost
-elements = []
-#categories
-categories = ["is", "not"]
-categories_all_elements = [categories for _ in range(total_posible_elements)]
-#fill elements
-for _ in range(total_posible_elements):
-    elements.append((rnd.uniform(0, max_value), rnd.uniform(0, max_cost)))
-
-
-def get_values_in(point, verbose):
-    sum_elements_in = 0
-    elements_value_in = []
-    elements_cost_in = []
-    for i in range(len(point)):
-        if point[i] == "is":
-            sum_elements_in +=1
-            elements_value_in.append(elements[i][0])
-            elements_cost_in.append(elements[i][1])
-    if verbose:
-        print("sum: ", sum_elements_in)
-        print("value total: ", sum(elements_value_in))
-        print("cost total: ", sum(elements_cost_in))
-        print("max knapsack capacity: ", knapsack_capacity)
-        print("values: ", elements_value_in)
-        print("costs: ", elements_cost_in)
-    return sum_elements_in, elements_value_in, elements_cost_in
-
-
-def is_valid(point) -> bool:
-    total_sum = 0
-    for i in range(len(point)):
-        if point[i] == "is":
-            total_sum += elements[i][1]
-    if total_sum > knapsack_capacity:
-        return False
-    return True
-
-def knapsack_obj(point) -> float:
-    if not is_valid(point):
-        return False
-    total_sum = 0
-    for i in range(len(point)):
-        if point[i] == "is":
-            total_sum += elements[i][0]
-    return total_sum
-
-def knapsack_repair(point):
-    i = -1
-    times_founded = 0
-    while not is_valid(point):
-        found = False
-        while not found:
-            i += 1
-            if i >= len(point):
-                print("que pedo wey, i es mayor que len en repair")
-                print("i value: ", i)
-                print("times founded: ", times_founded)
-                print("punto: ", point)
-                sum_elements_in, elements_value_in, elements_cost_in = get_values_in(point, True)
-                print(sum_elements_in)
-                print(elements_value_in)
-                print(elements_cost_in)
-                exit()
-            if point[i] == "is":
-                point[i] = "not"
-                found = True
-                times_founded +=1
-    return point
-
+knpsk = Knapsack_Categorical(knapsack_capacity, total_posible_elements, seed=0, max_value=5, max_cost=6)
 
 print("Genetic")
 print("#"*40)
 print("#"*40)
-mh = GeneticMH_Categorical(categorics=categories_all_elements, ndims=total_posible_elements, to_max=True, objective_function=knapsack_obj, repair_function=knapsack_repair)
+mh = GeneticMH_Categorical(categorics=knpsk.get_possible_categories(), ndims=knpsk.total_posible_elements, to_max=True, objective_function=knpsk.objective_function, repair_function=knpsk.repair_function)
 fit, pt = mh.run(verbose=True)
 print("fitness:")
 print(fit)
@@ -104,12 +33,12 @@ print(pt)
 print("movements:")
 print(mh.movements)
 
-get_values_in(pt, True)
+knpsk.get_values_in(pt, True)
 
 print("#"*40)
 print("#"*40)
 print("Genetic with leap")
-mh = GeneticMH_Categorical_WithLeap(categorics=categories_all_elements, ndims=total_posible_elements, to_max=True, objective_function=knapsack_obj, repair_function=knapsack_repair)
+mh = GeneticMH_Categorical_WithLeap(categorics=knpsk.get_possible_categories(), ndims=knpsk.total_posible_elements, to_max=True, objective_function=knpsk.objective_function, repair_function=knpsk.repair_function)
 fit, pt = mh.run(verbose=True)
 print("fitness:")
 print(fit)
@@ -118,4 +47,4 @@ print(pt)
 print("movements:")
 print(mh.movements)
 
-get_values_in(pt, True)
+knpsk.get_values_in(pt, True)
