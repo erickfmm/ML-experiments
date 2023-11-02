e6bd4cf06f7efd8d6091fd2d1e5a4ebd058c0649
Erick Merino <erick.merino@mineduc.cl>
2023-11-02 18:41:25 -0300

    interface of segmentation model

--- test/butterfly.py
+++ test/butterfly.py
@@ -154,7 +154,7 @@ def simple_segmentation(x_input, y_input):
     x = layers.UpSampling2D(size=(2,2))(x)
     x = layers.ZeroPadding2D(padding=(4, 4))(x)
     x = layers.concatenate([x, x1], axis=-1)
-    output = layers.Conv2D(3, 3, activation="sigmoid", padding="same")(x)
+    output = layers.Conv2D(3, 3, activation="tanh", padding="same")(x)
 
     model = keras.Model(inputs=[inputs], outputs=[output])
     model.summary() #only for printing purposes
@@ -163,15 +163,114 @@ def simple_segmentation(x_input, y_input):
         loss='binary_crossentropy',
         metrics=['accuracy']
         )
-    model.fit(x_input, y_input, epochs=100, batch_size=16)
+    model.fit(x_input, y_input, epochs=10, batch_size=16)
     return model
 
 
-if __name__ == "__main__":
-    print("running as main")
-    import os
-    print(os.getcwd())
-    x, x_seg, y = save_as_pickle(new_size=100, to_gray=False, to_pickle=False)
+def show_segmentation_interface(model, x_test, y_test):
+    pred = model.predict(x_test)
+    # Initialize the index variable
+    global idx
+    idx = 0
+    import tkinter as tk
+    from tkinter import filedialog
+    from PIL import Image, ImageTk
+    from matplotlib.figure import Figure
+    from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+    # Create a function to update the displayed images
+    def update_images():
+        ax1.clear()
+        ax1.imshow(pred[idx])
+        ax1.set_title('Predicted Image')
+        ax1.axis('off')
+        ax2.clear()
+        ax2.imshow(y_test[idx])
+        ax2.set_title('Actual Image')
+        ax2.axis('off')
+        ax3.clear()
+        ax3.imshow(x_test[idx])
+        ax3.set_title('Full Image')
+        ax3.axis('off')
+        ax4.clear()
+        ax4.imshow(x_test[idx]*pred[idx])
+        ax4.set_title('Cropped Image')
+        ax4.axis('off')
+        canvas.draw()
+
+    # Function to go to the previous image
+    def prev_image():
+        global idx
+        if idx > 0:
+            idx -= 1
+            update_images()
+
+    # Function to go to the next image
+    def next_image():
+        global idx
+        if idx < len(pred) - 1:
+            idx += 1
+            update_images()
+
+    # Function to upload an image and make a prediction
+    def upload_image():
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.ppm *.pgm")])
+        if file_path:
+            image = Image.open(file_path)
+            image = image.resize((100, 100))  # Resize the image to match model input size
+            image = np.array(image) / 255.0  # Normalize the image
+            prediction = model.predict(np.expand_dims(image, axis=0))[0]
+            idx = 0  # Reset the index
+            ax3.clear()
+            ax3.imshow(image)
+            ax3.set_title('Uploaded Image')
+            ax3.axis('off')
+
+            ax1.clear()
+            ax1.imshow(prediction)
+            ax1.set_title('Predicted Segmentation')
+            ax1.axis('off')
+
+            ax2.clear()
+            ax2.axis('off')
+
+            ax4.clear()
+            ax4.imshow(prediction*image)
+            ax4.set_title('Cropped Segmentation')
+            ax4.axis('off')
+            
+            canvas.draw()
+    # Create the main application window
+    root = tk.Tk()
+    root.title("Image Viewer")
+
+    # Create Matplotlib figure
+    fig = Figure(figsize=(8, 4), dpi=100)
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax2 = fig.add_subplot(2, 2, 2)
+
+    ax3 = fig.add_subplot(2, 2, 3)
+    ax4 = fig.add_subplot(2, 2, 4)
+
+    # Create a Tkinter canvas for the Matplotlib figure
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack()
+
+    # Create navigation buttons
+    prev_button = tk.Button(root, text="Previous", command=prev_image)
+    next_button = tk.Button(root, text="Next", command=next_image)
+    upload_button = tk.Button(root, text="Upload Image", command=upload_image)
+    prev_button.pack()
+    next_button.pack()
+    upload_button.pack()
+
+    # Display the initial image
+    update_images()
+
+    # Start the Tkinter main loop
+    root.mainloop()
+
+def segmentation_pipeline(x, x_seg):
     x_seg = np.asarray(x_seg)
     x_seg = np.nan_to_num(x_seg)
     x = np.asarray(x)
@@ -179,10 +278,23 @@ if __name__ == "__main__":
     print("len x ",len(x))
     print("shape x", x.shape)
     print("shape x_seg", x_seg.shape)
-    #x = tf.constant(x)
-    #x_seg = tf.constant(x_seg)
+    x = x/255.0
+    x_seg = x_seg/255.0
     x_train, x_test, y_train, y_test = train_test_split(x, x_seg, test_size=0.33, random_state=1992)
-    #simple_classifier_pipeline(x, y)
     print("x_train shape", x_train.shape)
     print("y_train shape", y_train.shape)
-    model = simple_segmentation(x_train, y_train)
+    print("x_test shape", x_test.shape)
+    print("y_test shape", y_test.shape)
+    filepath = "data/created_models/butterfly_segmentation.keras"
+    if os.path.exists(filepath):
+        model = keras.saving.load_model(filepath)
+    else:
+        model = simple_segmentation(x_train, y_train)
+        model.save(filepath)
+    show_segmentation_interface(model, x_test, y_test)
+
+if __name__ == "__main__":
+    print("running as main")
+    x, x_seg, y = save_as_pickle(new_size=100, to_gray=False, to_pickle=False)
+    #simple_classifier_pipeline(x, y)
+    segmentation_pipeline(x, x_seg)
